67e3bafd904232dd2a932b9b832074d7113ccb2a
BALLLONG <phuriwatsaranao@gmail.com>
2023-09-20 18:14:54 +0700

    page 127

---
+++ client/src/components/Users/Index.vue
@@ -0,0 +1,26 @@
+<template>
+        <div>
+            <h2>Get all users</h2>
+            <h4>จำนวนผู้ใช้งาน {{ users.length }}</h4>
+            <div v-for="user in users" v-bind:key="user.id">
+                <p>id: {{ user.id }}</p>
+                <p>ชื่อ-นามสกุล : {{ user.name }} - {{ user.lastname }}</p>
+                <p>email: {{ user.email }}</p>
+                <p>password: {{ user.password }}</p>
+            </div>
+        </div>
+    </template>
+<script>
+import UsersService from '@/services/UsersService'
+export default {
+    data() {
+        return {
+            users: []
+        }
+    },
+    async created() {
+        this.users = (await UsersService.index()).data
+    }
+}
+</script>
+<style scoped></style>
